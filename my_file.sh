5ed6419163cc7a4eeb4b16af958fd4e392fe374b
Austin Rivera <122767887+austin-t-rivera@users.noreply.github.com>
2025-09-16 19:22:39 -0400

    removing my_file.sh

---
+++ my_file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash​
+
+# $0 holds the name of the script itself​
+echo "Script name: $0"​
+
+
+# $1 holds the first argument​
+echo "First argument: $1"​
+
+
+# $2 holds the second argument​
+
+echo "Second argument: $2"​
+
+
+
+# $3 holds the third argument​
+
+echo "Third argument: $3"​
+
+
+# $@ holds all the arguments starting from $1​
+echo "All arguments: $@"​
+
+
+# $# holds the total number of arguments​
+echo "Number of arguments: $#"
