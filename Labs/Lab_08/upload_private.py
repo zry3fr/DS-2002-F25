24d7c411ecdb570825bfddfec4963ec222633a3c
zry3fr <zry3fr@virginia.edu>
2025-11-12 17:17:28 -0500

    Completed Lab 8: S3 Storage

--- Labs/Lab_08/upload_private.py
+++ Labs/Lab_08/upload_private.py
@@ -3,8 +3,8 @@ import boto3
 s3 = boto3.client('s3', region_name='us-east-1')
 
 bucket = 'ds2002-f25-zry3fr'
-local_file = 'PNG_transparency_demonstration_1.png'  # Replace with your file
-s3_key = 'private_image.png'  # This is how it will appear in S3
+local_file = 'PNG_transparency_demonstration_1.png' 
+s3_key = 'private_image.png' 
 
 with open(local_file, 'rb') as data:
     s3.put_object(Body=data, Bucket=bucket, Key=s3_key)
